094785a44cdbf5c45c28a3c5ecec4af54c0e9468
Bandaru srinivas <bandarusrinivas626@gmail.com>
2026-06-19 12:24:47 +0530

    done some changes

--- services/mailService.js
+++ services/mailService.js
@@ -4,11 +4,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+// Verify connection when server starts
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("Mail Server Error:");
+    console.log(error);
+  } else {
+    console.log("Mail Server Ready");
+  }
 });
 
 export default transporter;
